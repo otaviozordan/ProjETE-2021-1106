0ad8be0db06b2e909c13c8aa865d152e80a2fcd0
Otávio Zordan Alves <90804464+otaviozordan@users.noreply.github.com>
2021-10-26 22:27:06 -0300

    Update Slave - V1.ino

--- Slave_-_V1/Slave - V1.ino
+++ Slave_-_V1/Slave - V1.ino
@@ -5,52 +5,27 @@ const int stepsPerRevolution = 200;  // Define o numero de passo por volta (de a
 Stepper myStepper(stepsPerRevolution, 6, 7, 8, 9); //Declara motor nos pinos 6 à 9
 
 String recive;
-long dift, diff, dif, k;
-
+float gama;
 
 void setup() {
   myStepper.setSpeed(60); //Define a velocidade de rotação do motor para 60 rpm
   
   Serial.begin(9600); //Inicia comunicação serial
   
-  pinMode(5, OUTPUT);
-  digitalWrite(5, HIGH);
-  k = 0;
+  pinMode(10, INPUT);
+
 }
 
 void loop() {
 
-  if (Serial.available()){
-    recive = Serial.read();    
+  if (Serial.available() && digitalRead(10) == 1){
+    recive = Serial.readString(); 
+    Serial.println("Corriindo");   
+    myStepper.step(gama*360);
   }
 
-  if (recive == "Diferenca traseira de: ") {
-    Serial.println("Diferenca traseira recebida: ");    
-    while(!Serial.available()){}  
-  
-    if(Serial.available()){
-       dift == Serial.read();
-    }
-  } 
-
-  if (recive == "Diferenca frontal de: ") {
-    Serial.println("Diferenca recebida");    
-    while(!Serial.available()){}  
-  
-    if(Serial.available()){
-       diff == Serial.read();
-    }
-  } 
-
-  if (recive == "Diferenca entre eixos de: ") {
-    Serial.println("Diferenca entre eixos receida");    
-    while(!Serial.available()){}  
-  
-    if(Serial.available()){
-       dif == Serial.read();
-    }
-  } 
+ 
   delay(1);
-  myStepper.step(k+1);
+
   
 }
